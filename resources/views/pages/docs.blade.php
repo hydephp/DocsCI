9510fe2b4265a7f7b983bbad88ed62218830ca7d
hydephp <101899576+hydephp@users.noreply.github.com>
2022-03-31 07:06:51 +0200

    Remove the old redirect file

--- resources/views/pages/docs.blade.php
+++
@@ -1,7 +0,0 @@
-<meta http-equiv="refresh" content="0;url=/docs/master/index.html" />
-
-Redirecting you to the latest version.
-
-<br>
-
-Nothing happening? Try visiting <a href="https://hydephp.github.io/docs/master/index.html">https://hydephp.github.io/docs/master/index.html</a>
