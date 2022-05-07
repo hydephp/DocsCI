e7f589712a7e11a5c7119fad3e90356638f5a31b
Caen De Silva <caen@desilva.se>
2022-05-07 16:37:41 +0200

    Grammar

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -170,7 +170,7 @@
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path>
 						</svg>
 						<span class="font-medium text-gray-600 dark:text-gray-300">
-							Laravel developers wanting build a static site without having to learn a new framework.
+							Laravel developers wanting to build a static site without having to learn a new framework.
 						</span>
 					</li>
 					<li class="flex items-center py-2 space-x-4 xl:py-3">
@@ -187,7 +187,7 @@
 					xmlns="http://www.w3.org/2000/svg">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
 				</svg>
-				<span class="font-medium text-gray-600 dark:text-gray-300">Developers who wants to get documentation site up and running quickly, allowing them to focus on content.</span>
+				<span class="font-medium text-gray-600 dark:text-gray-300">Developers who want to get a documentation site up and running quickly, allowing them to focus on content.</span>
 			</li>
 		</ul>
 	</div>
