e782b6cf30c65cca596cad422e4df3518d2160c4
Caen De Silva <caen@desilva.se>
2022-05-20 14:57:05 +0200

    Remove unused app.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,8 @@
 
 let mix = require('laravel-mix');
 
-mix.js('resources/assets/app.js', 'app.js')
+mix
+	//.js('resources/assets/app.js', 'app.js')
 	.postCss('resources/assets/app.css', 'app.css', [
 	require('tailwindcss'),
 	require('autoprefixer'),
