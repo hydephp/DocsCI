96a57dcba7640ced480586b276b13246704c9168
Caen De Silva <95144705+caendesilva@users.noreply.github.com>
2022-06-11 17:07:54 +0200

    Add testamonial HTML

---
+++ resources/views/sections/testamonials.blade.php
@@ -0,0 +1,33 @@
+<section>
+    <header>
+        <h2>
+            Testamonials
+        </h2>
+        <p>
+            * Redacted links and names untill I get permission from them.
+        </p>
+    </header>
+    <article>
+        <header>
+            <address>
+                <a href="https://twitter.com/peter..." rel="author">Peter</a>
+            </address>
+            <date>June 10th</date>
+        </header>
+        <blockquote>
+            <p>
+                HydePHP is actually simple to setup.
+                Now, I'm not a PHP developer and I can barely write a function in this language,
+                but the project actually delivers on what it promises.
+            </p>
+            <p>
+                Docs: 10/10<br>
+                Project: 10/10<br>
+            </p>
+        </blockquote>
+        <footer>
+            <a href="https://twitter.com/peter.../status/123...">
+                See on Twitter</a>
+        </footer>
+    </article>
+</section>
